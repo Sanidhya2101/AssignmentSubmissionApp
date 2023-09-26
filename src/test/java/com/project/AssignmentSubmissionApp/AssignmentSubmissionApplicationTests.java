c36db738ed6a5a5e6aaa0775143df63841436ec5
Sanidhya2101 <58555534+Sanidhya2101@users.noreply.github.com>
2023-09-26 22:38:24 +0530

    First Commit: Basic SpringBoot Configuration

---
+++ src/test/java/com/project/AssignmentSubmissionApp/AssignmentSubmissionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.AssignmentSubmissionApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentSubmissionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
